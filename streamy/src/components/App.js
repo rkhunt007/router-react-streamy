5774e3e343a25d73ddaa059bc4e30f074d7de788
Rahul Khunt <rahul.khunt@techuplabs.com>
2021-05-01 16:01:07 +0530

    Setting up custom history object

--- streamy/src/components/App.js
+++ streamy/src/components/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Route, Router } from 'react-router-dom';
 import Header from "./Header";
+import history from './history';
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -8,7 +9,7 @@ import StreamShow from "./streams/StreamShow";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
         <div className="ui container">
             <Header></Header>
             <Route path="/" exact component={StreamList}></Route>
@@ -17,7 +18,7 @@ function App() {
             <Route path="/streams/delete" component={StreamDelete}></Route>
             <Route path="/streams/show" component={StreamShow}></Route>
         </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
